3333e9e7bdccbd32cb1be0824e4f8184d6bee7a1
mrwang01 <47618277+wastu01@users.noreply.github.com>
2022-03-09 18:04:26 +0800

    Todolist

---
+++ script.js
@@ -0,0 +1,28 @@
+/* please use Big-5 code open cuz school server can not use with UTF-8 */
+
+ function Newlist() {
+    ul.innerHTML += "<li>" + input.value + "</li>";
+
+  }
+
+    
+//   btn.addEventListener('click',function(e){
+//       alert(btn.textContent);
+//     }) 
+// 測試用
+
+// 待處理：空值不能新增
+// 且輸入完後要清除文字
+// 重新整理後，儲存資料還在
+// 新增了，那怎麼刪除？
+// 優化使用者體驗
+// 有時間再做～
+
+    
+
+  
+  
+  
+  
+
+
